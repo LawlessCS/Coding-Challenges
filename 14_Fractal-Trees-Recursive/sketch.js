06283e8490eaaa28228f53df4b0e84a1309a1f73
LawlessCS <37148937+LawlessCS@users.noreply.github.com>
2019-09-04 21:04:01 +0100

    Added fractal trees

--- 14_Fractal-Trees-Recursive/sketch.js
+++ 14_Fractal-Trees-Recursive/sketch.js
@@ -1,7 +1,33 @@
-function setup() {
+let a;
+let slider;
 
+function setup() {
+    createCanvas(400, 400);
+
+    a = PI / 4;
+    slider = createSlider(0, PI, PI / 4, 0.01);
 }
 
 function draw() {
-    
+    background(51);
+
+    a = slider.value();
+
+    stroke(255);
+    translate(200, height);
+    branch(100);
+}
+
+function branch(length) {
+    line(0, 0, 0, -length);
+    translate(0, -length);
+
+    if (length > 4) {
+        push();
+        rotate(a);
+        branch(length * 0.7);
+        pop();
+        rotate(-a);
+        branch(length * 0.7);
+    }
 }
