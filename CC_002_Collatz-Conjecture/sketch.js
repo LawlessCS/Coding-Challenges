666015ce786fcf180aab7ed0fa5acd0ca2e4deeb
Charles Batten <charlie@charliebatten.co.uk>
2019-10-27 17:58:21 +0100

    Added CC_002 Collatz Conjecture

--- CC_002_Collatz-Conjecture/sketch.js
+++ CC_002_Collatz-Conjecture/sketch.js
@@ -1,13 +1,42 @@
-let counter = 0;
+const len = 5;
+const angle = 0.15;
 
 function setup() {
-    createCanvas(400, 400);
+	createCanvas(600, 600);
+	background(0);
+
+	stroke(255, 10);
+	strokeWeight(4);
+
+	for (let i = 1; i < 10000; i++) {
+		let sequence = [];
+		let n = i;
+
+		do {
+			sequence.push(n);
+			n = collatz(n);
+		} while (n != 1);
+
+		sequence.push(1);
+		sequence.reverse();
+
+		resetMatrix();
+		translate(0, height / 2);
+
+		for (let j = 0; j < sequence.length; j++) {
+			let value = sequence[j];
+
+			rotate(value % 2 == 0 ? -angle : angle);
+			line(0, 0, len, 0);
+			translate(len, 0);
+		}
+	}
 }
 
-function draw() {
-    let backColour = map(0.5 + 0.5 * sin(counter / 25), 0, 1, 0.25, 0.75);
-    backColour *= 255;
-    background(backColour);
-
-    counter++;
+function collatz(n) {
+	if (n % 2 == 0) {
+		return n / 2;
+	} else {
+		return (3 * n + 1) / 2;
+	}
 }
