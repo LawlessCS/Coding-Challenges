e3472de7ada0e78aa3ad2bcb5f8ef25ea3917da7
Charlie B <charlie@charliebatten.co.uk>
2020-03-08 17:09:35 +0000

    Updated template

--- 000_TEMPLATE/sketch.js
+++ 000_TEMPLATE/sketch.js
@@ -2,13 +2,9 @@ let counter = 0;
 
 function setup() {
     createCanvas(400, 400);
-	console.log("P5 Loaded!");
 }
 
 function draw() {
-    let backColour = map(0.5 + 0.5 * sin(counter / 25), 0, 1, 0.25, 0.75);
-    backColour *= 255;
-    background(backColour);
-
+    background(map(Math.sin(0.1 * counter), -1,  1, 50, 200));
     counter++;
 }
