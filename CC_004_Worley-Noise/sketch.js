3f62779b63bcbb47b48a662d79c914cf654b6ffd
Charlie B <charlie@charliebatten.co.uk>
2020-05-25 18:19:11 +0100

    Added colour and video samples

--- CC_004_Worley-Noise/sketch.js
+++ CC_004_Worley-Noise/sketch.js
@@ -1,17 +1,25 @@
-const NUM_POINTS = 25;
+const NUM_POINTS = 30;
 const N = 1;
-const rValue = 2500;
+const rValue = 150000;
+
+const numSlices = 1;
+let z = 0;
 
 let points = [];
 
 function setup() {
-    createCanvas(250, 250);
+    createCanvas(1600, 900);
     background(0);
+    frameRate(1);
 
     // Fill points array
     for (let i = 0; i < NUM_POINTS; i++) {
         // Push object with coords to points array
-        points.push({ x: floor(random(width)), y: floor(random(height)) });
+        points.push({
+            x: floor(random(width)),
+            y: floor(random(height)),
+            z: floor(random(numSlices)),
+        });
     }
 
     // Set pixel density to 1 to negate page zoom
@@ -21,25 +29,31 @@ function setup() {
 }
 
 function draw() {
-    for (let y = 0; y < height; y++) {
-        for (let x = 0; x < width; x++) {
-            let index = getPixelIndex(x, y);
+    if (z == numSlices) {
+        noLoop();
+    } else {
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+                let index = getPixelIndex(x, y);
+                let distances = [];
+                for (let pt of points) {
+                    distances.push(hypotSquared(pt.x - x, pt.y - y, pt.z - z));
+                }
 
-            let distances = [];
-            for (let point of points) {
-                distances.push(hypotSquared(point.x - x, point.y - y));
+                distances.sort((a, b) => a - b);
+
+                let r = map(distances[N - 1], 0, rValue * 0.5, 0, 255);
+                let g = map(distances[N], 0, rValue, 0, 255);
+                let b = map(distances[N + 1], 0, rValue * 1.5, 0, 255);
+
+                setPixel(index, getColour(r, g, b));
             }
-
-            distances.sort((a, b) => a - b);
-
-            let noise = map(distances[N - 1], 0, rValue, 0, 255);
-
-            setPixel(index, getColour(noise));
         }
+        // Update canvas pixels to match 'pixels' array
+        updatePixels();
+        //save("image_" + nf(z, 3, 0) + ".png");
+        z++;
     }
-
-    // Update canvas pixels to match 'pixels' array
-    updatePixels();
 }
 
 function getPixelIndex(x, y) {
@@ -53,6 +67,10 @@ function setPixel(index, colour) {
     pixels[index + 3] = colour.a;
 }
 
-function hypotSquared(a, b) {
-    return Math.abs(a * a + b * b);
+function hypotSquared(a, b, c) {
+    if (c == null) {
+        return Math.abs(a * a + b * b);
+    } else {
+        return Math.abs(a * a + b * b + c * c);
+    }
 }
